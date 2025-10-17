56040fb094409f897d9ff405b1c9547b29970fae
Paolo Antonio Rossi <paolo@luduvigo.com>
2025-10-17 23:29:54 +0200

    check: remove lint error

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import Sidebar from "./components/Sidebar";
-import Header from "./components/Header";
+// import Header from "./components/Header";
 import Dashboard from "./components/Dashboard";
 
 export default function Home() {
