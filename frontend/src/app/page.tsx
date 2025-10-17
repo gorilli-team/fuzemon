83aceb708f8b4a6995157281476cef15b6a9dd9d
Paolo Antonio Rossi <paolo@luduvigo.com>
2025-10-18 00:42:10 +0200

    ui: show header in dashboard

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import Sidebar from "./components/Sidebar";
-// import Header from "./components/Header";
+import Header from "./components/Header";
 import Dashboard from "./components/Dashboard";
 
 export default function Home() {
@@ -19,7 +19,7 @@ export default function Home() {
           setSelectedPage={setSelectedPage}
         />
         <div className="flex-1 flex flex-col">
-          {/* <Header /> */}
+          <Header />
           <main className="flex-1 bg-dark-900">{renderContent()}</main>
         </div>
       </div>
