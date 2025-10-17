3fee5b1f11e4c23a851726e0f7d273880c70a866
Paolo Antonio Rossi <paolo@luduvigo.com>
2025-10-17 23:29:33 +0200

    check: remove header experiment

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
           setSelectedPage={setSelectedPage}
         />
         <div className="flex-1 flex flex-col">
-          <Header />
+          {/* <Header /> */}
           <main className="flex-1 bg-dark-900">{renderContent()}</main>
         </div>
       </div>
